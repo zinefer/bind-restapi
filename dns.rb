38526b9699869741c349e324d257f03a3dfb4cee
zinefer <zinefer@gmail.com>
2018-02-10 09:07:11 -0700

    bundler, puma, api key not required, ip not required for delete

--- dns.rb
+++ dns.rb
@@ -4,16 +4,21 @@ require 'rubygems'
 require 'sinatra'
 require 'json'
 require 'ipaddr'
+require 'socket'
 
 # curl -X POST -H 'Content-Type: application/json' -H 'X-Api-Key: secret' -d '{ "hostname": "host12.apple.com", "ip": "1.1.1.12" }' http://localhost:4567/dns
 # curl -X DELETE -H 'Content-Type: application/json' -H 'X-Api-Key: secret' -d '{ "hostname": "host12.apple.com", "ip": "1.1.1.12" }' http://localhost:4567/dns
 
 dns_params = {
-  :server => '127.0.0.1',
+  :server => 'localhost',
   :rndc_key => 'rndc-key',
-  :rndc_secret => 'MHGIj19UPT5EmWeHWFJLOw==',
-  :ttl => '300'
+  :rndc_secret => 'cf5hrPFanCzrsr7K6TbwBA==',
+  :ttl => '300',
+  :protected => false
 }
+
+set :bind, '0.0.0.0'
+set :port, 80
 
 # Reverse the IP address for the in-addr.arpa zone
 def reverse_ip(ipaddress)
@@ -24,7 +29,8 @@ end
 # Authenticate all requests with an API key
 before do
   # X-Api-Key
-  error 401 unless env['HTTP_X_API_KEY'] =~ /secret/
+    return unless dns_params[:protected]
+    error 401 unless env['HTTP_X_API_KEY'] =~ /secret/
 end
 
 post '/dns' do
@@ -49,7 +55,8 @@ end
 
 delete '/dns' do
   request_params = JSON.parse(request.body.read)
-  reverse_zone = reverse_ip(request_params["ip"])
+  ip = request_params["ip"] || IPSocket::getaddress(request_params["hostname"])
+  reverse_zone = reverse_ip(ip)
 
   # Remove record from forward and reverse zones, via TCP
   IO.popen("nsupdate -y #{dns_params[:rndc_key]}:#{dns_params[:rndc_secret]} -v", 'r+') do |f|
